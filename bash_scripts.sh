a06c70cef90ef8fb3cc8801fa6719011df936299
Mobeen Qamar <165556583+Mobeen-Dev@users.noreply.github.com>
2025-07-12 13:04:23 +0500

    added scripts

--- bash_scripts.sh
+++ bash_scripts.sh
@@ -1,3 +1,16 @@
-docker pull qdrant/qdrant
+# Prerequisites:
+    # Disable memory paging and swapping performance on the host to improve performance.
+    sudo swapoff -a
 
-docker run -p 6333:6333 -p 6334:6334  -v "./DB:/qdrant/storage:z"  qdrant/qdrant
+    sudo nano /etc/sysctl.conf
+    vm.max_map_count=262144
+    sudo sysctl -p
+    cat /sys/vm/max_map_count
+
+
+# Image Required
+    sudo docker pull qdrant/qdrant
+    sudo docker pull opensearchproject/opensearch:3
+    sudo docker pull opensearchproject/opensearch-dashboards:3
+
+sudo docker run -p 6333:6333 -p 6334:6334  -v "./DB:/qdrant/storage:z"  qdrant/qdrant
